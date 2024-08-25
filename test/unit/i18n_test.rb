d1cb146dc764193e2cab340934dcce6c1a7ebcc8
Alexander Meindl <a.meindl@alphanodes.com>
2024-08-25 19:24:22 +0200

    Fixed rubocop offenses

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -19,7 +19,7 @@ class I18nTest < ActiveSupport::TestCase
   end
 
   def test_locales_validness
-    lang_files_count = Dir[Rails.root.join('plugins/redmine_messenger/config/locales/*.yml')].size
+    lang_files_count = Rails.root.glob('plugins/redmine_messenger/config/locales/*.yml').size
 
     assert_equal 7, lang_files_count
     valid_languages.each do |lang|
