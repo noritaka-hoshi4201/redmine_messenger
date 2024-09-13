1256ef40da35fe46516ee9f64f2a3bb631d357d2
Alexander Meindl <a.meindl@alphanodes.com>
2024-09-13 19:52:06 +0200

    use field_is_default translation

--- lib/redmine_messenger/helpers.rb
+++ lib/redmine_messenger/helpers.rb
@@ -3,7 +3,7 @@
 module RedmineMessenger
   module Helpers
     def project_messenger_options(active)
-      options_for_select({ l(:label_messenger_settings_default) => '0',
+      options_for_select({ l(:field_is_default) => '0',
                            l(:label_messenger_settings_disabled) => '1',
                            l(:label_messenger_settings_enabled) => '2' }, active)
     end
